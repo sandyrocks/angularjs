43e2c6bc3d43f4f87e3e0b0f15064cffbd1cd819
Kavita <kavitaror1@gmail.com>
2015-02-17 17:09:22 -0500

    Added seed file for default user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+@user = User.find_or_create_by(email: "jwt@example.com", password_hash: "jwt123")
+Post.create(title: "To Buy a good house", user_id: @user.id)
+Post.create(title: "To Travel the world", user_id: @user.id)
+Post.create(title: "To Write a beautiful code", user_id: @user.id)
